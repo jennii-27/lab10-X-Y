49f7f9dff84c70e3a0f9eb00fa29cfab784d0363
YamirkaV <yamirkavarela@gmail.com>
2025-04-09 15:15:56 -0400

    modified calculator p1

--- calculator.py
+++ calculator.py
@@ -9,3 +9,28 @@ def add(a, b):
     pass
 
 
+import math
+def add(a, b):
+    return a + b
+
+
+def sub(a, b):
+    return a - b
+
+def mul(a, b):
+    return a * b
+
+def div(a, b):
+    if a == 0
+        raise ZeroDivisionError("Division by zero")
+    return a / b
+
+def log(a, b):
+    if a == 0 or a ==1 or b == 0:
+        raise ValueError("Cannot calculate log of zero")
+        # use math library + raise ValueError
+    return math.log(a, b)
+def exp(a, b):
+    return a**b
+
+
